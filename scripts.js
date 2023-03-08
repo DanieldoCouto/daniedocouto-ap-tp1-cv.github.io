a3e1aba37e6c36a26cc17214372b48f520fee968
DanieldoCouto <127341060+DanieldoCouto@users.noreply.github.com>
2023-03-08 15:44:20 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+function miMensajeMostrar(){
+
+    document.getElementById("miText").style.color = "rgb(126,110,92)"; 
+}
+
+function miMensajeOcultar(){
+
+    document.getElementById("miText").style.color = "rgb(248,249,251)"; 
+}
+
+function enviarMail(){
+   
+
+}
